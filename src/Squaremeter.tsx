156e06d632b449a17aea709a9ffc9cbdbd857c6f
Daniel Brandl <dtb.brandl@live.de>
2021-05-30 01:01:47 +0200

    planned, not yet working, components added

---
+++ src/Squaremeter.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Squaremeter() {
+    return (
+        <div>Squaremeter</div>
+    )
+}
---
+++ src/Testobot.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Testobot() {
+    return (
+        <div>Testobot</div>
+    )
+}
